5fc6eea750e495f3cc342873fe252824548a8893
Sergey Mezinov <s3rgh@mail.ru>
2021-01-10 22:47:26 +0300

    Stage 4/6: complete

--- Main.java
+++ Main.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sourceBase = scanner.nextInt();
         int number = scanner.nextInt();
-        int base = scanner.nextInt();
-        switch (base) {
-            case 2:
-                System.out.printf("0b%s", Long.toString(number, base));
-                break;
-            case 8:
-                System.out.printf("0%s", Long.toString(number, base));
-                break;
-            case 16:
-                System.out.printf("0x%s", Long.toString(number, base));
-                break;
-            default:
+        int newBase = scanner.nextInt();
+        int decimalNumber;
+        if (sourceBase == 1 && newBase == 1) {
+            System.out.println(number);
+        } else if (sourceBase == 10 && newBase == 1) {
+            System.out.print("1".repeat(number));
+        } else if (sourceBase == 1) {
+            decimalNumber = String.valueOf(number).length();
+            System.out.println(Integer.toString(decimalNumber, newBase));
+        }  else {
+            decimalNumber = Integer.parseInt(String.valueOf(number), sourceBase);
+            System.out.println(Integer.toString(decimalNumber, newBase));
         }
     }
 }
