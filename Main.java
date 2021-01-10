06e7596cb62ea844e9f939a90479813ee86c545b
Sergey Mezinov <s3rgh@mail.ru>
2021-01-10 20:04:38 +0300

    Stage 3/6 complete

--- Main.java
+++ Main.java
@@ -6,7 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int answer = number % 8;
-        System.out.println(answer);
+        int base = scanner.nextInt();
+        switch (base) {
+            case 2:
+                System.out.printf("0b%s", Long.toString(number, base));
+                break;
+            case 8:
+                System.out.printf("0%s", Long.toString(number, base));
+                break;
+            case 16:
+                System.out.printf("0x%s", Long.toString(number, base));
+                break;
+            default:
+        }
     }
 }
