d7e5f18599c117ed5f9e5883caea30af966d54af
Sergey Mezinov <s3rgh@mail.ru>
2021-01-12 20:12:36 +0300

    Stage 6/6: complete

--- Main.java
+++ Main.java
@@ -5,9 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int sourceBase = scanner.nextInt();
+        int sourceBase = 0;
+        try {
+            sourceBase = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("error");
+        }
         String number = scanner.next();
-        int newBase = scanner.nextInt();
-        DigitConverter.convertStrategy(sourceBase, number, newBase);
+        int newBase = 0;
+        try {
+            newBase = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+        if (sourceBase < 1 || sourceBase > 36 || newBase < 1 || newBase > 36) {
+            System.out.println("error");
+        } else {
+            DigitConverter.convertStrategy(sourceBase, number, newBase);
+        }
     }
 }
